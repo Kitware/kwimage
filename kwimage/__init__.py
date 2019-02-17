5791d80d6a75a4f59dc7b83b420a341c264b365f
joncrall <erotemic@gmail.com>
2019-02-16 21:44:15 -0500

    wip

--- kwimage/__init__.py
+++ kwimage/__init__.py
@@ -1,1 +1,20 @@
-__version__ = '0.0.1.dev0'
+from kwil.imutil.im_alphablend import (ensure_alpha_channel,
+                                       overlay_alpha_images,
+                                       overlay_alpha_layers,)
+from kwil.imutil.im_core import (atleast_3channels, ensure_float01,
+                                 ensure_uint255, make_channels_comparable,
+                                 num_channels,)
+from kwil.imutil.im_cv2 import (convert_colorspace, draw_boxes_on_image,
+                                draw_text_on_image, gaussian_patch, imscale,)
+from kwil.imutil.im_demodata import (grab_test_image, grab_test_image_fpath,)
+from kwil.imutil.im_io import (imread, imwrite,)
+from kwil.imutil.im_misc import (decode_run_length, encode_run_length,)
+from kwil.imutil.im_stack import (stack_images, stack_images_grid,)
+
+__all__ = ['atleast_3channels', 'convert_colorspace', 'decode_run_length',
+           'draw_boxes_on_image', 'draw_text_on_image', 'encode_run_length',
+           'ensure_alpha_channel', 'ensure_float01', 'ensure_uint255',
+           'gaussian_patch', 'grab_test_image', 'grab_test_image_fpath',
+           'imread', 'imscale', 'imwrite', 'make_channels_comparable',
+           'num_channels', 'overlay_alpha_images', 'overlay_alpha_layers',
+           'stack_images', 'stack_images_grid']
