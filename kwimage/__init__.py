12f96f8cdca6f3a6040ba58b4e8376dbf11b4f76
joncrall <erotemic@gmail.com>
2019-02-16 21:55:18 -0500

    wip

--- kwimage/__init__.py
+++ kwimage/__init__.py
@@ -1,20 +1,39 @@
-from kwil.imutil.im_alphablend import (ensure_alpha_channel,
-                                       overlay_alpha_images,
-                                       overlay_alpha_layers,)
-from kwil.imutil.im_core import (atleast_3channels, ensure_float01,
-                                 ensure_uint255, make_channels_comparable,
-                                 num_channels,)
-from kwil.imutil.im_cv2 import (convert_colorspace, draw_boxes_on_image,
-                                draw_text_on_image, gaussian_patch, imscale,)
-from kwil.imutil.im_demodata import (grab_test_image, grab_test_image_fpath,)
-from kwil.imutil.im_io import (imread, imwrite,)
-from kwil.imutil.im_misc import (decode_run_length, encode_run_length,)
-from kwil.imutil.im_stack import (stack_images, stack_images_grid,)
+"""
+mkinit ~/code/kwimage/kwimage/__init__.py
+"""
+
+from kwimage import im_alphablend
+from kwimage import im_core
+from kwimage import im_cv2
+from kwimage import im_demodata
+from kwimage import im_io
+from kwimage import im_misc
+from kwimage import im_stack
+from kwimage import util_warp
+
+from kwimage.im_alphablend import (ensure_alpha_channel, overlay_alpha_images,
+                                   overlay_alpha_layers,)
+from kwimage.im_core import (atleast_3channels, ensure_float01, ensure_uint255,
+                             make_channels_comparable, num_channels,)
+from kwimage.im_cv2 import (convert_colorspace, draw_boxes_on_image,
+                            draw_text_on_image, gaussian_patch, imscale,)
+from kwimage.im_demodata import (grab_test_image, grab_test_image_fpath,)
+from kwimage.im_io import (imread, imwrite,)
+from kwimage.im_misc import (decode_run_length, encode_run_length,)
+from kwimage.im_stack import (stack_images, stack_images_grid,)
+from kwimage.util_warp import (subpixel_accum, subpixel_align,
+                               subpixel_maximum, subpixel_minimum,
+                               subpixel_slice, subpixel_translate,
+                               warp_tensor,)
 
 __all__ = ['atleast_3channels', 'convert_colorspace', 'decode_run_length',
            'draw_boxes_on_image', 'draw_text_on_image', 'encode_run_length',
            'ensure_alpha_channel', 'ensure_float01', 'ensure_uint255',
            'gaussian_patch', 'grab_test_image', 'grab_test_image_fpath',
-           'imread', 'imscale', 'imwrite', 'make_channels_comparable',
-           'num_channels', 'overlay_alpha_images', 'overlay_alpha_layers',
-           'stack_images', 'stack_images_grid']
+           'im_alphablend', 'im_core', 'im_cv2', 'im_demodata', 'im_io',
+           'im_misc', 'im_stack', 'imread', 'imscale', 'imwrite',
+           'make_channels_comparable', 'num_channels', 'overlay_alpha_images',
+           'overlay_alpha_layers', 'stack_images', 'stack_images_grid',
+           'subpixel_accum', 'subpixel_align', 'subpixel_maximum',
+           'subpixel_minimum', 'subpixel_slice', 'subpixel_translate',
+           'util_warp', 'warp_tensor']
