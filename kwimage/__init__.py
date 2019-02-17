757c230d4d8a3544a61ee1fac02746c9e984bc5b
joncrall <erotemic@gmail.com>
2019-02-16 22:20:20 -0500

    wip

--- kwimage/__init__.py
+++ kwimage/__init__.py
@@ -1,32 +1,31 @@
 """
-mkinit ~/code/kwimage/kwimage/__init__.py
+mkinit ~/code/kwimage/kwimage/__init__.py --relative -w
 """
 
 __version__ = '0.0.1'
 
-from kwimage import im_alphablend
-from kwimage import im_core
-from kwimage import im_cv2
-from kwimage import im_demodata
-from kwimage import im_io
-from kwimage import im_misc
-from kwimage import im_stack
-from kwimage import util_warp
+from . import im_alphablend
+from . import im_core
+from . import im_cv2
+from . import im_demodata
+from . import im_io
+from . import im_misc
+from . import im_stack
+from . import util_warp
 
-from kwimage.im_alphablend import (ensure_alpha_channel, overlay_alpha_images,
-                                   overlay_alpha_layers,)
-from kwimage.im_core import (atleast_3channels, ensure_float01, ensure_uint255,
-                             make_channels_comparable, num_channels,)
-from kwimage.im_cv2 import (convert_colorspace, draw_boxes_on_image,
-                            draw_text_on_image, gaussian_patch, imscale,)
-from kwimage.im_demodata import (grab_test_image, grab_test_image_fpath,)
-from kwimage.im_io import (imread, imwrite,)
-from kwimage.im_misc import (decode_run_length, encode_run_length,)
-from kwimage.im_stack import (stack_images, stack_images_grid,)
-from kwimage.util_warp import (subpixel_accum, subpixel_align,
-                               subpixel_maximum, subpixel_minimum,
-                               subpixel_slice, subpixel_translate,
-                               warp_tensor,)
+from .im_alphablend import (ensure_alpha_channel, overlay_alpha_images,
+                            overlay_alpha_layers,)
+from .im_core import (atleast_3channels, ensure_float01, ensure_uint255,
+                      make_channels_comparable, num_channels,)
+from .im_cv2 import (convert_colorspace, draw_boxes_on_image,
+                     draw_text_on_image, gaussian_patch, imscale,)
+from .im_demodata import (grab_test_image, grab_test_image_fpath,)
+from .im_io import (imread, imwrite,)
+from .im_misc import (decode_run_length, encode_run_length,)
+from .im_stack import (stack_images, stack_images_grid,)
+from .util_warp import (subpixel_accum, subpixel_align, subpixel_maximum,
+                        subpixel_minimum, subpixel_slice, subpixel_translate,
+                        warp_tensor,)
 
 __all__ = ['atleast_3channels', 'convert_colorspace', 'decode_run_length',
            'draw_boxes_on_image', 'draw_text_on_image', 'encode_run_length',
