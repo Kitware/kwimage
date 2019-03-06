a17ffb5cb2d5e3ce2bd11b22b130f459839ce40f
joncrall <jon.crall@kitware.com>
2019-03-06 09:35:55 -0500

    Add drone yaml

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Update Requirments:
+    # Requirements are broken down by type in the `requirements` folder, and
+    # `requirments.txt` lists them all. Thus we autogenerate via:
+
+    cat requirements/*.txt | sort -u | grep -o '^[^#]*' >  requirements.txt
+"""
 from os.path import exists
 from os.path import join
 from skbuild import setup
