23629108e221e2ad9d64274c396ba191f973f5e9
joncrall <erotemic@gmail.com>
2023-07-07 14:15:35 -0400

    xcookie update

--- run_linter.sh
+++ run_linter.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-flake8 ./kwimage --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 --count --select=E9,F63,F7,F82 --show-source --statistics kwimage
+flake8 --count --select=E9,F63,F7,F82 --show-source --statistics ./tests
