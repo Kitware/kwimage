ae3361ab4b86c10a1791834b641228a8ed819d62
joncrall <jon.crall@kitware.com>
2026-03-10 16:50:19 -0400

    fix type checking

--- kwimage/_typing.py
+++ kwimage/_typing.py
@@ -1,19 +1,21 @@
 # Helpers for typing
 
 from __future__ import annotations
-from typing import TYPE_CHECKING, Callable, TypeAlias
-from numpy.typing import ArrayLike
+from typing import TYPE_CHECKING
 from skimage.transform import _geometric
-from kwimage.transform import Transform
 if TYPE_CHECKING:
     from typing import Any
+    from typing import Callable
+    from numpy.typing import ArrayLike
+    from kwimage.transform import Transform
 
 SKImageGeometricTransform: Any = getattr(_geometric, '_GeometricTransform', None)
 if SKImageGeometricTransform is None:
     # Older version compatability
     SKImageGeometricTransform = getattr(_geometric, 'GeometricTransform')
 
-TransformLike: TypeAlias = SKImageGeometricTransform | ArrayLike | Callable | Transform
+if TYPE_CHECKING:
+    TransformLike = SKImageGeometricTransform | ArrayLike | Callable | Transform
 
 __all__ = [
     'SKImageGeometricTransform',
