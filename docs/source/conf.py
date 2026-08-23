e10767fd870fc200df905917fcd6cddb7c5481a6
joncrall <jon.crall@kitware.com>
2026-08-23 18:42:48 -0400

    Update xcookie, remove setup.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,9 +111,9 @@ Notes:
 # sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
-from os.path import dirname, exists, join
-
-import sphinx_rtd_theme
+from os.path import exists
+from os.path import dirname
+from os.path import join
 
 
 def parse_version(fpath):
@@ -132,7 +132,7 @@ def parse_version(fpath):
         def visit_Assign(self, node):
             for target in node.targets:
                 if getattr(target, 'id', None) == '__version__':
-                    self.version = node.value.s
+                    self.version = node.value.value
 
     visitor = VersionVisitor()
     visitor.visit(pt)
@@ -180,6 +180,7 @@ extensions = [
 ]
 
 todo_include_todos = True
+myst_heading_anchors = 3
 napoleon_google_docstring = True
 napoleon_use_param = False
 napoleon_use_ivar = True
@@ -235,6 +236,7 @@ intersphinx_mapping = {
     'xdoctest': ('https://xdoctest.readthedocs.io/en/latest/', None),
     'networkx': ('https://networkx.org/documentation/stable/', None),
     'scriptconfig': ('https://scriptconfig.readthedocs.io/en/latest/', None),
+    'kwconf': ('https://kwconf.readthedocs.io/en/latest/', None),
     'rich': ('https://rich.readthedocs.io/en/latest/', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
     'sympy': ('https://docs.sympy.org/latest/', None),
@@ -270,10 +272,12 @@ python -m sphinx.ext.intersphinx https://pygments-doc.readthedocs.io/en/latest/o
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-source_suffix = ['.rst', '.md']
+# The suffix(es) of source filenames. Mapping form avoids Sphinx
+# converting a legacy list at runtime.
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = 'index'
@@ -300,7 +304,6 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -308,7 +311,6 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #
 html_theme_options = {
     'collapse_navigation': False,
-    'display_version': True,
     'navigation_depth': -1,
     # 'logo_only': True,
 }
@@ -318,7 +320,10 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# Keep empty by default to avoid warnings when generated projects do not
+# define custom static assets. Projects that create ``docs/source/_static``
+# can opt back in locally.
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -426,7 +431,7 @@ class PatchedPythonDomain(PythonDomain):
     """
 
     def resolve_xref(
-        self, env, fromdocname, builder, typ, target, node, contnode
+        self, env, fromdocname, builder, type, target, node, contnode
     ):
         """
         Helps to resolves cross-references
@@ -436,7 +441,7 @@ class PatchedPythonDomain(PythonDomain):
         if target.startswith('xdoc.'):
             target = 'xdoctest.' + target[3]
         return_value = super(PatchedPythonDomain, self).resolve_xref(
-            env, fromdocname, builder, typ, target, node, contnode
+            env, fromdocname, builder, type, target, node, contnode
         )
         return return_value
 
@@ -833,6 +838,7 @@ def create_doctest_figure(app, obj, name, lines):
     import types
 
     import xdoctest
+    import xdoctest.core
 
     if isinstance(obj, types.ModuleType):
         module = obj
@@ -1055,6 +1061,7 @@ def fix_rst_todo_section(lines):
 
 def setup(app):
     import sphinx
+    import sphinx.application
 
     app: sphinx.application.Sphinx = app
     app.add_domain(PatchedPythonDomain, override=True)
